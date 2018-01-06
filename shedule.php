d5e841c7b1349aed52f112c69c644434380de928
Mike0712 <barrister2003@gmail.com>
2018-01-06 12:12:45 +0300

    task 5

--- shedule.php
+++ shedule.php
@@ -1,3 +1,14 @@
+#!/usr/bin/php
 <?php
+use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
+require __DIR__.'/vendor/autoload.php';
 
-echo 'Super thing';
+$app = require_once __DIR__.'/bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$kernel->handle(
+    $request = Illuminate\Http\Request::createFromBase(SymfonyRequest::create('/cron'))
+);
+
+VisitCounter::notificate('Вася', 'vasily@mail.ru');
